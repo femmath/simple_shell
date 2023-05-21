84b1b23b816f0da8f319dede018896f8ca2e0b54
gloriaadanna <gloriaagwam7@gmail.com>
2023-05-21 03:48:18 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+
+#endif
